9f0dae59d63c92441c767c2677c6ac80680d6c5b
giang00yb <ggvu2017@gmail.com>
2021-05-15 23:28:53 +0700

    update

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const songs = [
+  "Robber and Bouquet",
+  "Spring Thief",
+  "Creation",
+  "Eat the Wind",
+  "Liar",
+  "FISHANDCHIPS",
+  "Shion",
+  "Gravity",
+  "Ikutoshitsuki",
+  "Alkalemia",
+];
+
+const artists = [
+  "Yorushika",
+  "Yorushika",
+  "Yorushika",
+  "Yorushika",
+  "Yorushika",
+  "Toko Miura",
+  "yourness",
+  "BUMP OF CHICKEN",
+  "Mili",
+  "mol-74",
+];
